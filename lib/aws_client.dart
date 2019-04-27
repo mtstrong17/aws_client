6dbc1870d627020830eded2a80390decc0f8527b
Michael Strong <michael@fyresite.com>
2019-04-26 18:17:21 -0700

    started converting clients

--- lib/aws_client.dart
+++ lib/aws_client.dart
@@ -5,25 +5,23 @@
 import 'package:http_client/http_client.dart' as http;
 
 import 'src/credentials.dart';
+import 'src/config.dart';
 import 'sqs.dart';
+
+export './src/config.dart';
+export './src/credentials.dart';
 
 /// AWS access facade that helps to initialize multiple services with common
 /// settings (credentials and HTTP client).
-class Aws {
-  final Credentials _credentials;
-  final http.Client _httpClient;
+class AWS {
+  AWSConfig config;
 
   Sqs _sqs;
 
   ///
-  Aws({Credentials credentials, http.Client httpClient})
-      : _credentials = credentials,
-        _httpClient = httpClient {
-    assert(this._credentials != null);
-    assert(this._httpClient != null);
-  }
+  AWS([this.config]);
 
   /// Returns an SQS service, inheriting the properties of this instance.
-  Sqs get sqs =>
-      _sqs ??= new Sqs(credentials: _credentials, httpClient: _httpClient);
+  // Sqs get sqs =>
+  //     _sqs ??= new Sqs(credentials: _credentials, httpClient: _httpClient);
 }
